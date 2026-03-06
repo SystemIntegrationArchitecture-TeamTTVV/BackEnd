9e002a471de5fafa696ccff9e77a0ee791c17a32
Tống Phan Kim Thạch <22637901.thach@student.iuh.edu.vn>
2026-03-06 15:02:20 +0700

    fix run

--- CommonService/src/main/java/edu/iuh/fit/se/commonservice/config/RestTemplateConfig.java
+++ CommonService/src/main/java/edu/iuh/fit/se/commonservice/config/RestTemplateConfig.java
@@ -1,6 +1,5 @@
 package edu.iuh.fit.se.commonservice.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -11,11 +10,6 @@ public class RestTemplateConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
-    }
-
-    @Bean
-    public ObjectMapper objectMapper() {
-        return new ObjectMapper();
     }
 }
 
